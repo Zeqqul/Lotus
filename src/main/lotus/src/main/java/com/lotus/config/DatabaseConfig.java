c5f5f48a0910bfbcdb44c07ff9f0ca4a5c931219
Hojosa (Dani Ruano) <djrruano@gmail.com>
2024-08-26 21:52:31 +0100

    feat: add H2 database dependency and initial configuration

---
+++ src/main/lotus/src/main/java/com/lotus/config/DatabaseConfig.java
@@ -0,0 +1,11 @@
+package com.example.myproject.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig () {
+    private static final String JDBC_URL = "";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "mamahuevo";
+}
