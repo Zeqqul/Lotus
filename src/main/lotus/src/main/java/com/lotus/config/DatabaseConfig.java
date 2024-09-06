0716b22bc5fab1725674239e55a0b035ea818cef
Raharth <ra641@kent.ac.uk>
2024-09-06 18:29:37 +0100

    chore: fix error in databaseconfig

--- src/main/lotus/src/main/java/com/lotus/config/DatabaseConfig.java
+++ src/main/lotus/src/main/java/com/lotus/config/DatabaseConfig.java
@@ -1,10 +1,10 @@
-package com.example.myproject.config;
+package com.lotus.config;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseConfig () {
+public class DatabaseConfig{
     private static final String JDBC_URL = "";
     private static final String USER = "admin";
     private static final String PASSWORD = "mamahuevo";
